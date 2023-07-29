18f22af237d5b5f3add65f767969cc2f93a4d85c
Alex Jenkins <108849441+ajenkinsynwa@users.noreply.github.com>
2023-07-28 21:38:51 -0400

    user update

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import Hero from './componets/Hero';
+import Demo from './componets/Demo';
+import './App.css';
 
 const App = () => {
   return (
-    <div>app</div>
+    <main>
+      <div> app </div>
+    <div className='main'>
+      <div className='gradient' />
+    </div>
+
+    <div className='app'>
+      <Hero />
+      <Demo />
+
+    </div>
+    </main>
   )
 }
 
---
+++ src/componets/Hero.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { logo } from '../assets';
+
+const Hero = () => {
+  return (
+    <header className='w-full flex justify-center items-center flex-col'>
+      <nav className="flex justify-between items-center flex-col'
+        items-center flex-col">
+          <img src={logo} alt='sumz_logo'
+          className='w-28 object-contain' />
+     
+
+      <button 
+      type = 'button' 
+      onclick={() => window.open('https://github.com/ajenkinsynwa')}
+      className='black_btn'
+    > GitHub
+    </button>
+    </nav>
+    </header>
+  
+  )
+}
+
+export default Hero
